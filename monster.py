c8adddeede50f1fccf9c71d6adde74569762efbc
ltekean <ltekean@gmail.com>
2023-03-28 16:05:22 +0900

    assassin 2턴마다 스킬 두번 나가게끔 사용 assassin이 나약하다는 제보를 받고 급히 밸런스 패치 class 부분 교통정리

--- monster.py
+++ monster.py
@@ -14,25 +14,7 @@ class Character:
         self.mp = mp
         self.power = power
         self.magic_power = magic_power
-
-    # 플레이어의 물리딜
-    def attack(self, other):
-        damage = random.randint(self.power - 2, self.power + 2)
-        other.hp = max(other.hp - damage, 0)
-        print(f"{self.name}의 공격! {other.name}에게 {damage}의 데미지를 입혔습니다.")
-        if other.hp == 0:
-            print(f"{other.name}이(가) 쓰러졌습니다.")
-
-    # 몬스터의 물리딜
-    def monster_attack(self, other):
-        monster_damage = random.randint(self.power - 1, self.power + 3)
-        other.hp = max(other.hp - monster_damage, 0)
-        print(f"{other.name}의 공격! {self.name}에게 {monster_damage}의 데미지를 입혔습니다.")
-        if self.hp == 0:
-            print(f"{self.name}이(가) 쓰러졌습니다.")
-    # 몬스터 함수이니까 몬스터 클래스에 넣어서 오버라이딩 해도 좋을것 같아요, 그러면 damage변수를 monster_damage로 새로 쓸 필요없이 그대로 damage로 써도 됩니다.
-    # 또 self, other는 항상 함수가 속해있는 클래스 기준이기 때문에 다시 바꿔서 써주시면 될것 같습니다.
-
+    
     #상태창(플레이어)
     def player_show_status(self):
         print(f"{self.name}의 상태: HP {int(self.hp)}/{self.max_hp} MP {self.mp}/{self.max_mp}")
@@ -45,6 +27,14 @@ class Character:
 class Player(Character):
     # 모체 캐릭터의 성능을 가진 자식 플레이어의 특성
 
+    # 플레이어의 물리딜
+    def attack(self, other):
+        damage = random.randint(self.power - 2, self.power + 2)
+        other.hp = max(other.hp - damage, 0)
+        print(f"{self.name}의 공격! {other.name}에게 {damage}의 데미지를 입혔습니다.")
+        if other.hp == 0:
+            print(f"{other.name}이(가) 쓰러졌습니다.")
+
     # 플레이어의 마법딜
     def magic_attack(self, other):
         magic_damage = random.randint(self.magic_power + 6, self.magic_power + 12)
@@ -52,16 +42,16 @@ class Player(Character):
         self.mp = max(self.mp - 10, 0)
 
         print(f"{self.name}의 공격! {other.name}에게 {magic_damage}의 데미지를 입혔습니다.")
-        if other.hp == 0:
+        if other.hp <= 0:
             print(f"{other.name}이(가) 쓰러졌습니다.")
         
 class Monster(Character):
     # 모체 캐릭터의 성능을 가진 자식 몬스터의 특성 ##
     def monster_attack(self, other):
-        damage = random.randint(self.power - 1, self.power + 3)
+        damage = random.randint(self.power - 2, self.power + 2)
         other.hp = max(other.hp - damage, 0)
         print(f"{self.name}의 공격! {other.name}에게 {damage}의 데미지를 입혔습니다.")
-        if self.hp == 0:
+        if self.hp <= 0:
             print(f"{other.name}이(가) 쓰러졌습니다.")
 
 
@@ -69,12 +59,8 @@ class Monster(Character):
 print("플레이어의 이름을 입력하세요!!")
 Player_name = input("Player name: ") 
 
-# #각 캐릭터별 능력치 (name, hp, mp, power, magic_power)                  #아래에서 클래스를 지정해주기 때문에 주석처리했습니다!
-# P1 = Player('Warrior', 100, 40, 15, 20)
-# P2 = Player('Assassin', 70, 40, 25, 15)
-# P3 = Player('Wizard', 80, 60, 10, 30)
-
-m1 = Monster('Killer', 120, 0, 20, 0)                #몬스터는 그대로 변수에 넣었습니다! M1으로 해도 상관없으실것 같은데 저는 파란색뜨는게 거슬려서 바꿨습니다!
+#몬스터 변수(고유값보단 소문자 넣어서 m1 넣는 게 맞는 듯)
+m1 = Monster('Killer', 350, 0, 30, 0)
 
 # 플레이어 1, 2, 3 중에 픽할 것
 print('플레이어를 선택하시오')
@@ -82,38 +68,50 @@ print('1. Warrior')
 print('2. Assassin')
 print('3. Wizard')
 
-# 번호값 input할 시 캐릭터 배정
+# 번호값 input할 시 캐릭터 배정(name, hp, mp, power, magic_power)
 Player_select = int(input('플레이어를 선택하시오. '))
 if Player_select == 1:
-    Character_name = Player('Warrior', 100, 40, 15, 20)
+    Character_name = Player('Warrior', 300, 80, 30, 28)
 elif Player_select == 2:
-    Character_name = Player('Assassin', 70, 40, 25, 15)
+    Character_name = Player('Assassin', 220, 70, 40, 25)
 elif Player_select == 3:
-    Character_name = Player('Wizard', 80, 60, 10, 30)
+    Character_name = Player('Wizard', 250, 80, 20, 40)
 print(f'{Player_name} 님의 캐릭터는 {Character_name.name}입니다.')
-                                                                # 여기서 바로 클래스를 지정해서 생성하면 좋을 것 같습니다.
-                                                                # Character_name = Player('Warrior', 100, 40, 15, 20) 이런식으로 하시면
-                                                                # 바로 클래스에서 인스턴스를 생성하면서 Character_name 변수에 넣을 수 있습니다.
+                                                                
 
 
 
 # turn을 짝수 홀수로 하여 플레이어와 몬스터의 순번을 정함
-turn = 1
+turn = 0
 
 # 플레이어 차례
 print('fight!')
 while True:
-    if turn % 2 == 1:
+    if turn % 2 == 0:
         print('My turn!')
         print('몬스터에게 어떤 공격을 하시겠습니까?')
         player_action = int(input("1.일반공격 2.마법공격 ")) #int빠짐. player_action이라는 함수명이 위에 있어서 변수명으로 작동하지 않았음.
         if player_action == 1:
-            Character_name.attack(m1) 
+            if Player_select == 2:
+                if turn % 4 == 2:
+                    Character_name.attack(m1)
+                    Character_name.attack(m1)
+                else:
+                    Character_name.attack(m1)
+            else:
+                Character_name.attack(m1) 
         elif player_action == 2:
             if Character_name.mp < 10:
                 print('마나도 없는 게 공격을?')
             else:
-                Character_name.magic_attack(m1)
+                if Player_select == 2:
+                    if turn % 4 ==2:
+                        Character_name.magic_attack(m1)
+                        Character_name.magic_attack(m1)
+                    else:
+                        Character_name.magic_attack(m1)
+                else:
+                    Character_name.magic_attack(m1)
 
         m1.monster_show_status()
 
@@ -125,7 +123,7 @@ while True:
             turn += 1
 
 # 몬스터 차례
-    elif turn % 2 == 0:
+    elif turn % 2 == 1:
         print('Monster turn!')
         m1.monster_attack(Character_name)
         Character_name.player_show_status()
@@ -153,3 +151,7 @@ while True:
 # 필수 추가 사항
 # 1. 한 쪽이 죽으면 턴제와 게임이 종료되도록 해야 함 = 해결 (if 문에 break를 쓰면 해결됨.)
 # 2. def show_status가 이루어져야 함 = 해결 Character_name.player_show_status(), m1.monster_show_status()
+
+# 추가 능력치 투입
+# 1. assassin은 두 번째 공격마다 멀티 딜이 들어간다.
+# 2. 
